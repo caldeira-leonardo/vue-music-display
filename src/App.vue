7d1898615a61ab578cade1ef52fc37bfcc92af1e
Leonardo Caldeira <leonardo.caldeira@cronosbank.com.br>
2024-09-11 10:30:25 -0300

    refactor(pages): refactored aoo to show aplication pages

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,10 @@
 <script setup>
-import { ref, watch, watchEffect, reactive, computed } from 'vue'
-import { useToast } from 'primevue/usetoast'
-
-const sum = computed(() => Number(counter?.value) + Number(multiple?.value))
-const counter = ref(0)
-const multiple = ref(0)
-const validations = reactive({
-  isPrime: false
-})
-
-const toast = useToast()
-
-function isPrimeNumber(num) {
-  if (num <= 1) return false
-  if (num <= 3) return true
-
-  if (num % 2 === 0 || num % 3 === 0) return false
-
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false
-  }
-
-  return true
-}
-
-const show = (counter, multiple) => {
-  toast.add({
-    severity: 'success',
-    summary: 'Multiplo encontrado',
-    detail: `${counter} é multiplo de ${multiple}`,
-    life: 3000
-  })
-}
-
-function showMultiple() {
-  if (counter.value > 0 && counter.value % multiple.value === 0) {
-    show(counter.value, multiple.value)
-  }
-}
-
-watch(counter, () => {
-  validations.isPrime = isPrimeNumber(counter.value)
-})
-
-watchEffect(showMultiple)
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex gap-10 p-20">
-    <div class="flex flex-col items-center">
-      <div class="flex">
-        <TheButton label="-" :onclick="() => counter--" />
-        <h1 class="mb-4 text-4xl font-extrabold w-60 flex justify-center">
-          Contador: {{ counter }}
-        </h1>
-        <TheButton label="+" :onclick="() => counter++" />
-      </div>
-
-      <h2>Número primo: {{ !!validations.isPrime ? 'Sim' : 'Não' }}</h2>
-    </div>
-
-    <FloatLabel>
-      <InputText if="Multiplo" v-model="multiple" />
-      <label for="Multiplo">Multiplo</label>
-    </FloatLabel>
-
-    <h1 class="text-2xl font-extrabold w-60 flex justify-center !w-[390px]">
-      A soma dos valores é de:
-      <span class="flex !ml-4">{{ sum }}</span>
-    </h1>
-  </div>
   <TheToast />
+  <RouterView />
 </template>
 
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/HomeView.vue')
+    },
+    {
+      path: '/counter',
+      name: 'counter',
+      component: () => import('@/views/ConterView.vue')
+    },
+    {
+      path: '/todo',
+      name: 'todo',
+      component: () => import('@/views/TodoView.vue')
+    }
+  ]
 })
 
 router.beforeEach((_, __, next) => {
